c21fdda53a2db99708661a743f296c3079efcafa
QuanTrieuPCYT <admin@qtpc.tech>
2022-12-21 17:22:30 +0700

    just small changes

--- src/main/java/de/flashyboi/minecraft/plugins/statisticswebserver/StatisticsWebserver.java
+++ src/main/java/de/flashyboi/minecraft/plugins/statisticswebserver/StatisticsWebserver.java
@@ -8,8 +8,6 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 
 public final class StatisticsWebserver extends JavaPlugin {
@@ -29,6 +27,8 @@ public final class StatisticsWebserver extends JavaPlugin {
 class WebserverManager {
     private static final String KILL_QUERY = "kills";
     private static final String DEATH_QUERY = "deaths";
+    private static final String PLAYTIME_QUERY = "playtime";
+    private static final String MOBKILL_QUERY = "mobkills";
     protected static void startWebserver(int port, String host) {
         Undertow server = Undertow.builder()
                 .addHttpListener(port, host)
@@ -37,22 +37,35 @@ class WebserverManager {
                             .put(Headers.CONTENT_TYPE, "text/html");
                     Map<String, Deque<String>> queryParams = exchange.getQueryParameters();
                     try {
-                        String uuidQuery = queryParams.get("UUID").element();
-                        UUID uuid = UUID.fromString(uuidQuery);
-                        System.out.println(uuid);
+                        String playerQuery = queryParams.get("player").element();
+                        // System.out.println(playerQuery);
                         String statisticQuery = queryParams.get("stat").element();
                         String response = "";
                         switch (statisticQuery) {
                             case KILL_QUERY:
                                 try {
-                                    response = PlayerStatisticsManager.getPlayerKills(uuid);
+                                    response = PlayerStatisticsManager.getPlayerKills(playerQuery);
                                 } catch (PlayerNotFoundException playerNotFoundException) {
                                     exchange.setStatusCode(404);
                                 }
                                 break;
                             case DEATH_QUERY:
                                 try {
-                                    response = PlayerStatisticsManager.getPlayerDeaths(uuid);
+                                    response = PlayerStatisticsManager.getPlayerDeaths(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                                break;
+                            case PLAYTIME_QUERY:
+                                try {
+                                    response = PlayerStatisticsManager.getPlayTime(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                                break;
+                            case MOBKILL_QUERY:
+                                try {
+                                    response = PlayerStatisticsManager.getMobKills(playerQuery);
                                 } catch (PlayerNotFoundException playerNotFoundException) {
                                     exchange.setStatusCode(404);
                                 }
@@ -73,10 +86,10 @@ class WebserverManager {
 }
 
 class PlayerStatisticsManager {
-    public static String getPlayerKills(UUID playerUuid) throws PlayerNotFoundException {
-        OfflinePlayer player = Bukkit.getOfflinePlayer(playerUuid);
+    public static String getPlayerKills(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
         try {
-            System.out.println(player.hasPlayedBefore());
+            // System.out.println(player.hasPlayedBefore());
             if (!player.hasPlayedBefore()) {
                 throw new PlayerNotFoundException();
             }
@@ -87,10 +100,10 @@ class PlayerStatisticsManager {
         }
     }
 
-    public static String getPlayerDeaths(UUID playerUuid) throws PlayerNotFoundException {
-        OfflinePlayer player = Bukkit.getOfflinePlayer(playerUuid);
+    public static String getPlayerDeaths(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
         try {
-            System.out.println(player.hasPlayedBefore());
+            // System.out.println(player.hasPlayedBefore());
             if (!player.hasPlayedBefore()) {
                 throw new PlayerNotFoundException();
             }
@@ -98,6 +111,33 @@ class PlayerStatisticsManager {
         } catch (NullPointerException npe) {
 
             return String.valueOf(player.getStatistic(Statistic.DEATHS));
+        }
+    }
+
+    public static String getPlayTime(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        try {
+            // System.out.println(player.hasPlayedBefore());
+            if (!player.hasPlayedBefore()) {
+                throw new PlayerNotFoundException();
+            }
+            return String.valueOf(player.getStatistic(Statistic.TOTAL_WORLD_TIME)*0.05);
+        } catch (NullPointerException npe) {
+
+            return String.valueOf(player.getStatistic(Statistic.TOTAL_WORLD_TIME)*0.05);
+        }
+    }
+    public static String getMobKills(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        try {
+            // System.out.println(player.hasPlayedBefore());
+            if (!player.hasPlayedBefore()) {
+                throw new PlayerNotFoundException();
+            }
+            return String.valueOf(player.getStatistic(Statistic.MOB_KILLS));
+        } catch (NullPointerException npe) {
+
+            return String.valueOf(player.getStatistic(Statistic.MOB_KILLS));
         }
     }
 }
