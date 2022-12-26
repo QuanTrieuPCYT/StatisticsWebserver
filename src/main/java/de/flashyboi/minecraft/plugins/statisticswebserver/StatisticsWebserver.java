f46e4ac163531687152093402e5c2ab68ec13238
QuanTrieuPCYT <admin@qtpc.tech>
2022-12-26 22:48:46 +0700

    SkyBlock additional changes

--- src/main/java/de/flashyboi/minecraft/plugins/statisticswebserver/StatisticsWebserver.java
+++ src/main/java/de/flashyboi/minecraft/plugins/statisticswebserver/StatisticsWebserver.java
@@ -1,5 +1,6 @@
 package de.flashyboi.minecraft.plugins.statisticswebserver;
 
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import de.flashyboi.minecraft.plugins.statisticswebserver.exceptions.PlayerNotFoundException;
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
@@ -9,6 +10,7 @@ import org.bukkit.Statistic;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import de.myzelyam.api.vanish.VanishAPI;
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
 
 import java.util.*;
 import java.util.logging.Logger;
@@ -44,6 +46,12 @@ class WebserverManager {
     private static final String MOBKILL_QUERY = "mobkills";
     private static final String LASTPLAYED_QUERY = "lastplayed";
     private static final String TIMESINCELASTONLINE_QUERY = "timesincelastonline";
+    private static final String ISLANDNAME_QUERY = "islandname";
+    private static final String ISLANDSCHEMATIC_QUERY = "islandschematic";
+    private static final String ISLANDOWNER_QUERY = "islandowner";
+    private static final String ISLANDLEVEL_QUERY = "islandlevel";
+    private static final String ISLANDMEMBERS_QUERY = "islandmembers";
+    private static final String ISLANDWORTH_QUERY = "islandworth";
     protected static void startWebserver(int port, String host) {
         Undertow server = Undertow.builder()
                 .addHttpListener(port, host)
@@ -94,6 +102,48 @@ class WebserverManager {
                             case TIMESINCELASTONLINE_QUERY -> {
                                 try {
                                     response = PlayerStatisticsManager.getTimeSinceLastOnline(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                            }
+                            case ISLANDNAME_QUERY -> {
+                                try {
+                                    response = PlayerStatisticsManager.getIslandName(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                            }
+                            case ISLANDSCHEMATIC_QUERY -> {
+                                try {
+                                    response = PlayerStatisticsManager.getIslandSchematicName(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                            }
+                            case ISLANDOWNER_QUERY -> {
+                                try {
+                                    response = PlayerStatisticsManager.getIslandOwnerName(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                            }
+                            case ISLANDLEVEL_QUERY -> {
+                                try {
+                                    response = PlayerStatisticsManager.getIslandLevel(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                            }
+                            case ISLANDMEMBERS_QUERY -> {
+                                try {
+                                    response = PlayerStatisticsManager.getIslandMembers(playerQuery);
+                                } catch (PlayerNotFoundException playerNotFoundException) {
+                                    exchange.setStatusCode(404);
+                                }
+                            }
+                            case ISLANDWORTH_QUERY -> {
+                                try {
+                                    response = PlayerStatisticsManager.getIslandWorth(playerQuery);
                                 } catch (PlayerNotFoundException playerNotFoundException) {
                                     exchange.setStatusCode(404);
                                 }
@@ -196,6 +246,87 @@ class PlayerStatisticsManager {
             } else {
                 return String.valueOf(System.currentTimeMillis() - player.getLastPlayed());
             }
+        }
+    }
+    public static String getIslandName(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        // We need to also bring SuperiorSkyblock2 into the mix
+        SuperiorPlayer skyplayer = SuperiorSkyblockAPI.getPlayer(playerName);
+        try {
+            // If the player hasn't joined, OR doesn't have an island
+            // returns 404 error code
+            if (!player.hasPlayedBefore() || !skyplayer.hasIsland()) {
+                throw new PlayerNotFoundException();
+            }
+            return Objects.requireNonNull(skyplayer.getIsland()).getName();
+        } catch (NullPointerException npe) {
+
+            return Objects.requireNonNull(skyplayer.getIsland()).getName();
+        }
+    }
+    public static String getIslandSchematicName(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        SuperiorPlayer skyplayer = SuperiorSkyblockAPI.getPlayer(playerName);
+        try {
+            if (!player.hasPlayedBefore() || !skyplayer.hasIsland()) {
+                throw new PlayerNotFoundException();
+            }
+            return Objects.requireNonNull(skyplayer.getIsland()).getSchematicName();
+        } catch (NullPointerException npe) {
+
+            return Objects.requireNonNull(skyplayer.getIsland()).getSchematicName();
+        }
+    }
+    public static String getIslandOwnerName(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        SuperiorPlayer skyplayer = SuperiorSkyblockAPI.getPlayer(playerName);
+        try {
+            if (!player.hasPlayedBefore() || !skyplayer.hasIsland()) {
+                throw new PlayerNotFoundException();
+            }
+            return Objects.requireNonNull(skyplayer.getIsland()).getOwner().getName();
+        } catch (NullPointerException npe) {
+
+            return Objects.requireNonNull(skyplayer.getIsland()).getOwner().getName();
+        }
+    }
+    public static String getIslandLevel(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        SuperiorPlayer skyplayer = SuperiorSkyblockAPI.getPlayer(playerName);
+        try {
+            if (!player.hasPlayedBefore() || !skyplayer.hasIsland()) {
+                throw new PlayerNotFoundException();
+            }
+            return String.valueOf(Objects.requireNonNull(skyplayer.getIsland()).getIslandLevel());
+        } catch (NullPointerException npe) {
+
+            return String.valueOf(Objects.requireNonNull(skyplayer.getIsland()).getIslandLevel());
+        }
+    }
+    public static String getIslandMembers(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        SuperiorPlayer skyplayer = SuperiorSkyblockAPI.getPlayer(playerName);
+        try {
+            if (!player.hasPlayedBefore() || !skyplayer.hasIsland()) {
+                throw new PlayerNotFoundException();
+            }
+            return Objects.requireNonNull(skyplayer.getIsland()).getIslandMembers().toString();
+        } catch (NullPointerException npe) {
+
+            return Objects.requireNonNull(skyplayer.getIsland()).getIslandMembers().toString();
+        }
+    }
+    public static String getIslandWorth(String playerName) throws PlayerNotFoundException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
+        SuperiorPlayer skyplayer = SuperiorSkyblockAPI.getPlayer(playerName);
+        try {
+            if (!player.hasPlayedBefore() || !skyplayer.hasIsland()) {
+                throw new PlayerNotFoundException();
+            }
+            return String.valueOf(Objects.requireNonNull(skyplayer.getIsland()).getWorth());
+        } catch (NullPointerException npe) {
+
+            return String.valueOf(Objects.requireNonNull(skyplayer.getIsland()).getWorth());
         }
     }
 }
